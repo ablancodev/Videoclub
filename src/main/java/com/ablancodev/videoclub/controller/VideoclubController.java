919bbeeefb15c7453f592b5b0a9941ca3ae13216
eggemplo <eggemplo@gmail.com>
2018-02-26 23:26:00 +0100

    Using get params

--- src/main/java/com/ablancodev/videoclub/controller/VideoclubController.java
+++ src/main/java/com/ablancodev/videoclub/controller/VideoclubController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,9 +23,19 @@ public class VideoclubController {
 		return mav;
 	}
 
+	/*
 	// .../movie?id=xx
 	@GetMapping( "/movie" )
 	public ModelAndView getMovie( @RequestParam( name="id", required=false, defaultValue="NULL" ) String id ) {
+		ModelAndView mav = new ModelAndView( "movie" );
+		mav.addObject( "idMovie", id );
+		return mav;
+	}
+	*/
+
+	// .../movie/xx
+	@GetMapping( "/movie/{id}" )
+	public ModelAndView getMovie( @PathVariable( "id" ) String id ) {
 		ModelAndView mav = new ModelAndView( "movie" );
 		mav.addObject( "idMovie", id );
 		return mav;
