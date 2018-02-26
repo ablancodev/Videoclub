c45912eb21674f02b2188cfc5651597bd7d6412b
eggemplo <eggemplo@gmail.com>
2018-02-26 19:35:30 +0100

    Initial version

---
+++ src/main/java/com/ablancodev/videoclub/VideoclubApplication.java
@@ -0,0 +1,12 @@
+package com.ablancodev.videoclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoclubApplication.class, args);
+	}
+}
